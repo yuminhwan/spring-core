4c281d923697981d24c165f298687243c3bf1ec1
yuminhwan <ymh7532@icloud.com>
2022-10-17 23:51:46 +0900

    빈 스코프: 싱글톤 & 프로토타입 함께 사용시 해결법 -  스프링 컨테이너 사용(Dependency Lookup)

--- src/test/java/com/example/core/scope/SingletonWithPrototypeTest.java
+++ src/test/java/com/example/core/scope/SingletonWithPrototypeTest.java
@@ -7,6 +7,8 @@ import javax.annotation.PreDestroy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -35,25 +37,23 @@ class SingletonWithPrototypeTest {
             PrototypeBean.class);
 
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
-        int count1 = clientBean1.login();
+        int count1 = clientBean1.logic();
         assertThat(count1).isEqualTo(1);
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
-        int count2 = clientBean2.login();
-        assertThat(count2).isEqualTo(2);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
 
         ac.close();
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; //  생성 시점에 주입
+        @Autowired
+        private ApplicationContext ac;
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
-
-        public int login() {
+        public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
