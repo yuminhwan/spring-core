b12082986d93d875679f6ea6462e372b14a2739f
yuminhwan <ymh7532@icloud.com>
2022-10-26 16:40:42 +0900

    서블릿: Hello 서블릿

---
+++ spring-mvc/src/main/java/com/example/springmvc/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.example.springmvc.basic;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.MediaType;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType(MediaType.TEXT_PLAIN_VALUE);
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
