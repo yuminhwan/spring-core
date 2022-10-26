62f9d03803a1a10a5d63f2aa549c438654048e54
yuminhwan <ymh7532@icloud.com>
2022-10-27 01:48:44 +0900

    MVC 프레임워크 만들기 : 프론트 컨트롤러 구현

---
+++ spring-mvc/src/main/java/com/example/springmvc/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -0,0 +1,29 @@
+package com.example.springmvc.web.frontcontroller.v1.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.springmvc.domain.member.Member;
+import com.example.springmvc.domain.member.MemberRepository;
+import com.example.springmvc.web.frontcontroller.v1.ControllerV1;
+
+public class MemberListControllerV1 implements ControllerV1 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
