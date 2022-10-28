174d7b48604215ae153e0d38044aa27332f65eaf
yuminhwan <ymh7532@icloud.com>
2022-10-29 00:26:57 +0900

    스프링 MVC: HTTP 파라미터 - @ModelAttribute

--- spring-mvc/src/main/java/com/example/springmvc/basic/request/RequestParamController.java
+++ spring-mvc/src/main/java/com/example/springmvc/basic/request/RequestParamController.java
@@ -7,9 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.springmvc.basic.HelloData;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -86,6 +89,25 @@ public class RequestParamController {
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         log.info("username = {}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttribute(@ModelAttribute HelloData helloData) {
+        log.info("username = {}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    /**
+     * @ModelAttribute 생략 가능
+     * String, int 같은 단순 타입 = @RequestParam
+     * argument resolver 로 지정해둔 타입 외 = @ModelAttribute */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(),
+            helloData.getAge());
         return "ok";
     }
 }
