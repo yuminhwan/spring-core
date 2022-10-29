9fc88907525816bc23dba171f099884819afa114
yuminhwan <ymh7532@icloud.com>
2022-10-29 16:37:24 +0900

    chore: 프로젝트 초기 설정

---
+++ java-8to11/src/main/java/com/example/java8to11/Java8to11Application.java
@@ -0,0 +1,13 @@
+package com.example.java8to11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8to11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8to11Application.class, args);
+	}
+
+}
---
+++ java-8to11/src/test/java/com/example/java8to11/Java8to11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java8to11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8to11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
