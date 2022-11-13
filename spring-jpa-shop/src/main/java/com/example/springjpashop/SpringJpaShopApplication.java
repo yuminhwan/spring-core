b206aba9898121d94a767add2c9a09913bc587d9
yuminhwan <ymh7532@icloud.com>
2022-11-13 18:01:33 +0900

    chore: 프로젝트 초기 설정

---
+++ spring-jpa-shop/src/main/java/com/example/springjpashop/SpringJpaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaShopApplication.class, args);
+    }
+
+}
---
+++ spring-jpa-shop/src/test/java/com/example/springjpashop/SpringJpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
