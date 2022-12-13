c3ad4db330d2257d54d2d414e72f9b18aa111dbe
yuminhwan <ymh7532@icloud.com>
2022-12-13 23:40:16 +0900

    chore: 프로젝트 초기 설정

---
+++ spring-mvc-web/src/main/java/com/example/springmvcweb/SpringMvcWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcWebApplication.class, args);
+	}
+
+}
