3e3bd5ff3f0a9f38c924fa09e8cc78caa66b1538
yuminhwan <ymh7532@icloud.com>
2023-01-17 22:30:35 +0900

    chore: 프로젝트 초기 설정

---
+++ spring-openfeign/src/main/java/com/example/openfeign/SpringOpenfeignApplication.java
@@ -0,0 +1,13 @@
+package com.example.openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenfeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenfeignApplication.class, args);
+	}
+
+}
---
+++ spring-openfeign/src/test/java/com/example/openfeign/SpringOpenfeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenfeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
