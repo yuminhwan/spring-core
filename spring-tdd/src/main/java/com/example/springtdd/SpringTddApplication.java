a14fc645847ec1b44abf0c9566b6b5ecf3ad692c
yuminhwan <ymh7532@icloud.com>
2023-01-29 01:31:28 +0900

    chore: 프로젝트 초기 설정

---
+++ spring-tdd/src/main/java/com/example/springtdd/SpringTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTddApplication.class, args);
+    }
+
+}
---
+++ spring-tdd/src/test/java/com/example/springtdd/SpringTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
