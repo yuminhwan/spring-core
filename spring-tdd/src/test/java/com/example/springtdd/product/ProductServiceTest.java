f7afa7843aaede993cc1ffb28d3769e978bd4f90
yuminhwan <ymh7532@icloud.com>
2023-02-01 03:39:10 +0900

    test: Stub 방식 테스트

--- spring-tdd/src/test/java/com/example/springtdd/product/ProductServiceTest.java
+++ spring-tdd/src/test/java/com/example/springtdd/product/ProductServiceTest.java
@@ -3,18 +3,21 @@ package com.example.springtdd.product;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * 검증 부분을 먼저 만드는 것이 쉽다!
  */
-@SpringBootTest
 class ProductServiceTest {
 
-    @Autowired
+    private final StubProductPort productPort = new StubProductPort();
     private ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+        productService = new ProductService(productPort);
+    }
 
     @Test
     void 상품등록() {
@@ -39,4 +42,32 @@ class ProductServiceTest {
         assertThat(response).isNotNull();
     }
 
+    @Test
+    void 상품수정() {
+        // given
+        final Long productId = 1L;
+        final UpdateProductRequest request = new UpdateProductRequest("상품 수정", 2000, DiscountPolicy.NONE);
+        final Product product = new Product("상품명", 1000, DiscountPolicy.NONE);
+        productPort.getProduct_will_return = product;
+
+        // when
+        productService.updateProduct(productId, request);
+
+        // then
+        assertThat(product.getName()).isEqualTo("상품 수정");
+    }
+
+    private static class StubProductPort implements ProductPort {
+        public Product getProduct_will_return;
+
+        @Override
+        public void save(final Product product) {
+
+        }
+
+        @Override
+        public Product getProduct(final Long productId) {
+            return getProduct_will_return;
+        }
+    }
 }
