f3426b2b047cfdb08c266ff501eb4081d0ade412
yuminhwan <ymh7532@icloud.com>
2023-02-01 03:51:17 +0900

    test: 상품 수정 API 테스트

--- spring-tdd/src/test/java/com/example/springtdd/product/ProductApiTest.java
+++ spring-tdd/src/test/java/com/example/springtdd/product/ProductApiTest.java
@@ -4,11 +4,18 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import com.example.springtdd.ApiTest;
 
+import io.restassured.RestAssured;
+
 class ProductApiTest extends ApiTest {
+
+    @Autowired
+    private ProductRepository productRepository;
 
     @Test
     void 상품등록() {
@@ -35,6 +42,28 @@ class ProductApiTest extends ApiTest {
         assertAll(
             () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
             () -> assertThat(response.jsonPath().getString("name")).isEqualTo("상품명")
+        );
+    }
+
+    @Test
+    void 상품수정() {
+        // given
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+        final Long productId = 1L;
+        final UpdateProductRequest request = ProductSteps.상품수정요청_생성();
+
+        // when
+        final var response = RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(request)
+            .patch("/products/{productId}", productId)
+            .then().log().all()
+            .extract();
+
+        // then
+        assertAll(
+            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+            () -> assertThat(productRepository.findById(1L).get().getPrice()).isEqualTo(2000)
         );
     }
 }
