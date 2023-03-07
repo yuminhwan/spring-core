5588331612ae44ec62bba7706618e9a88d7ab105
yuminhwan <ymh7532@icloud.com>
2023-03-08 01:54:10 +0900

    feat: 매핑 및 바인딩

--- spring-boot/src/main/java/com/example/springboot/Application.java
+++ spring-boot/src/main/java/com/example/springboot/Application.java
@@ -19,17 +19,22 @@ public class Application {
     public static void main(String[] args) {
         final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         final WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            final HelloController helloController = new HelloController();
+
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws
                     IOException {
                     // 인증, 보안, 다국어 등 공통 기능 처리
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        // 어떤 로직을 수행할 것인가 매핑
                         final String name = req.getParameter("name");
+
+                        final String ret = helloController.hello(name);  // 바인딩 -> 웹 기술 필요 XX, 분리
 
                         resp.setStatus(HttpStatus.OK.value());  // 상태 코드
                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // 헤더
-                        resp.getWriter().println("Hello " + name);  // 바디
+                        resp.getWriter().println(ret);  // 바디
                     } else if (req.getRequestURI().equals("/user")) {
                         // 처리
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
