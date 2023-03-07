4a2ebf5db29b279742c4ef17b75263712436333d
yuminhwan <ymh7532@icloud.com>
2023-03-08 01:38:04 +0900

    feat: 서블릿 등록

--- spring-boot/README.md
+++ spring-boot/README.md
@@ -6,3 +6,9 @@
   이를 쉽게 사용할 수 있도록 부트는 TomcatServletWebServerFactory와 같은 객체를 제공해준다.
     - 기본 설정은 되어 있음!
 - ServletWebServerFactory, Webserver 추상화 객체
+
+### 서블릿 등록
+
+- ServletContextInitializer -> servlet 등록을 도와주는 객체
+- HttpServlet -> 서블릿 구현 객체 , 어떤 동작을 할 지와 어디에 매핑될 지 명시해야함
+- 응답에는 상태 코드, 헤더, 바디가 있어야 한다. 
--- spring-boot/src/main/java/com/example/springboot/Application.java
+++ spring-boot/src/main/java/com/example/springboot/Application.java
@@ -1,14 +1,33 @@
 package com.example.springboot;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 public class Application {
 
     public static void main(String[] args) {
         final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        final WebServer webServer = serverFactory.getWebServer();
+        final WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws
+                    IOException {
+                    resp.setStatus(HttpStatus.OK.value());  // 상태 코드
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // 헤더
+                    resp.getWriter().println("Hello Servlet");  // 바디
+                }
+            }).addMapping("/hello");
+        });
         webServer.start();
     }
 
