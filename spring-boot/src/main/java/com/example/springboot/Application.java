fdb07d1f1fd7c60cd5725523ab250fd3daaf92c1
yuminhwan <ymh7532@icloud.com>
2023-03-08 01:47:58 +0900

    feat: 프론트 컨트롤러 전환

--- spring-boot/src/main/java/com/example/springboot/Application.java
+++ spring-boot/src/main/java/com/example/springboot/Application.java
@@ -10,6 +10,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -18,17 +19,25 @@ public class Application {
     public static void main(String[] args) {
         final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         final WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws
                     IOException {
-                    final String name = req.getParameter("name");
+                    // 인증, 보안, 다국어 등 공통 기능 처리
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        final String name = req.getParameter("name");
 
-                    resp.setStatus(HttpStatus.OK.value());  // 상태 코드
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // 헤더
-                    resp.getWriter().println("Hello " + name);  // 바디
+                        resp.setStatus(HttpStatus.OK.value());  // 상태 코드
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // 헤더
+                        resp.getWriter().println("Hello " + name);  // 바디
+                    } else if (req.getRequestURI().equals("/user")) {
+                        // 처리
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*");
         });
         webServer.start();
     }
