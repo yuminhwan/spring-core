047d72156b72ed335704336a79da8abb021af7b0
yuminhwan <ymh7532@icloud.com>
2023-03-09 03:39:03 +0900

    feat: DispatcherServlet 적용

--- spring-boot/src/main/java/com/example/springboot/Application.java
+++ spring-boot/src/main/java/com/example/springboot/Application.java
@@ -1,48 +1,24 @@
 package com.example.springboot;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class Application {
 
     public static void main(String[] args) {
-        final GenericApplicationContext applicationContext = new GenericApplicationContext();
+        final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
         final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         final WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws
-                    IOException {
-                    // 인증, 보안, 다국어 등 공통 기능 처리
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        final String name = req.getParameter("name");
-
-                        final HelloController helloController = applicationContext.getBean(HelloController.class);
-                        final String ret = helloController.hello(name);
-
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    } else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("dispatcherServlet",
+                new DispatcherServlet(applicationContext)
+            ).addMapping("/*");
         });
         webServer.start();
     }
--- spring-boot/src/main/java/com/example/springboot/HelloController.java
+++ spring-boot/src/main/java/com/example/springboot/HelloController.java
@@ -2,6 +2,11 @@ package com.example.springboot;
 
 import java.util.Objects;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -10,6 +15,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
