4acaeb425788969b3c0462ef451178e115d24a8d
yuminhwan <ymh7532@icloud.com>
2023-03-10 00:14:20 +0900

    feat: SpringBootApplication

--- spring-boot/src/main/java/com/example/springboot/Application.java
+++ spring-boot/src/main/java/com/example/springboot/Application.java
@@ -1,12 +1,11 @@
 package com.example.springboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -14,23 +13,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class Application {
 
     public static void main(String[] args) {
-        final AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                final ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                final DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-                final WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                        .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(Application.class);
-        applicationContext.refresh();
+        SpringApplication.run(Application.class, args);
     }
 
     @Bean
